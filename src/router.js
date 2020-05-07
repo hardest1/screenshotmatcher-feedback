143407a5d7011894bdbd14697ca1dd3ec8f0f9fd
Alex <alex_ha@live.de>
2020-05-07 07:25:58 +0200

    add basic auth to dashboard

--- src/router.js
+++ src/router.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const basicAuth = require('express-basic-auth')
 const router = express.Router()
+
+const config = require('../config')
 
 const dashboard = require('./dashboard')
 const feedback = require('./feedback')
 
-router.get('/', dashboard);
+const username = config.USERNAME;
+const password = config.PASSWORD;
+
+const users = {}
+users[username] = password
+
+router.get('/', basicAuth({ users, challenge: true }), dashboard);
 
 router.post('/feedback', feedback);
 
