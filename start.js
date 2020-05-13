72fe8d7f23787d91cd1ae6ff39bc19c6697cf420
root <root@localhost.localdomain>
2020-05-13 10:10:19 +0000

    add bodyparser / fix https

--- start.js
+++ start.js
@@ -4,11 +4,10 @@ const fs = require('fs');
 const fileUpload = require('express-fileupload');
 const http = require('http');
 const https = require('https');
+const bodyParser = require('body-parser');
 const app = express();
 
 const config = require('./config')
-
-const PORT = process.env.PORT || config.PORT || 3000;
 
 const privateKey  = fs.readFileSync('ssl/hartmann-it.de.key', 'utf8');
 const certificate = fs.readFileSync('ssl/hartmann-it.de.cer', 'utf8');
@@ -18,6 +17,8 @@ app.use((req, res, next) => {
   console.log( new Date().toLocaleDateString(), new Date().toLocaleTimeString(), req.method, req.originalUrl )
   next()
 })
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(fileUpload({
   limits: { fileSize: 10 * 1024 * 1024 },
@@ -34,10 +35,10 @@ app.use('/feedbacks', express.static(path.join(__dirname, 'feedbacks')))
 const httpsServer = https.createServer(sslCredentials, app);
 
 httpsServer.listen(config.FEEDBACK_PORT_HTTPS, () => {
-  console.log('Server listening on port ' + PORT);
+  console.log('HTTPS Server listening on port ' + config.FEEDBACK_PORT_HTTPS);
 });
 
 http.createServer(function (req, res) {
   res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
   res.end();
-}).listen(config.FEEDBACK_PORT_HTTP, () => console.log('HTTP Server listening'));
+}).listen(config.FEEDBACK_PORT_HTTP, () => console.log('HTTP Server listening on port ' + config.FEEDBACK_PORT_HTTP));
