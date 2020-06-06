deefe38786e4000e544e9347a6051cdd7b303de4
root <root@localhost.localdomain>
2020-06-06 05:21:34 +0000

    add logging route

---
+++ src/log.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const sha1 = require('sha1');
+const moment = require('moment');
+const config = require('../config');
+
+
+module.exports = async (req, res) => {
+
+  // exit if wrong secret provided
+  if(req.body.secret !== config.SECRET) return res.sendStatus(401);
+
+
+  let identifier = req.body.identifier || false;
+
+  // exit if no identifier provided
+  if(!identifier) return res.sendStatus(400);
+
+  const ipAddrRaw = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ipAddr = ipAddrRaw.split(':').slice(-1)[0]
+
+  // get data
+  let algorithm = req.body.algorithm || 'N/A';
+  let speed = req.body.speed || 'N/A';
+  let hasResult = req.body.hasResult || false;
+
+  let identifierHashed = sha1(identifier)
+
+  // calc filename with current time
+  const newFilename = config.LOG_DIR + '/log_' + identifierHashed + '.csv'
+
+  const logData = [
+    moment().format('DD.MM.YY HH:mm:ss'),
+    ipAddr,
+    hasResult,
+    algorithm,
+    speed,
+  ]
+
+  // append to log file
+  fs.appendFileSync(newFilename, logData.join(';') + '\n')
+  
+  res.sendStatus(200)
+
+}
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ const config = require('../config')
 
 const dashboard = require('./dashboard')
 const feedback = require('./feedback')
+const log = require('./log')
 
 const username = config.USERNAME;
 const password = config.PASSWORD;
@@ -16,5 +17,6 @@ users[username] = password
 router.get('/', basicAuth({ users, challenge: true }), dashboard);
 
 router.post('/feedback', feedback);
+router.post('/log', log);
 
 module.exports = router
