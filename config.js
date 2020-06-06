d0f6cba689938b4fd06c956cdb0822d9696accd0
root <root@localhost.localdomain>
2020-06-06 05:19:16 +0000

    add log dir to config

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   FEEDBACK_PORT_HTTP: process.env.FEEDBACK_PORT_HTTP || 80,
   FEEDBACK_PORT_HTTPS: process.env.FEEDBACK_PORT_HTTPS || 443,
   FEEDBACK_DIR: __dirname + '/feedbacks',
+  LOG_DIR: __dirname + '/logs',
   SECRET: process.env.FEEDBACK_SECRET,
   USERNAME: process.env.FEEDBACK_USER,
   PASSWORD: process.env.FEEDBACK_PASSWORD,
